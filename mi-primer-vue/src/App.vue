1b7821ba08a36529d1ea6355fe4917714a57e026
luisr <luisr@DESKTOP-4NLA5R0>
2023-01-05 22:47:20 +0100

    ok

--- mi-primer-vue/src/App.vue
+++ mi-primer-vue/src/App.vue
@@ -1,4 +1,18 @@
+<script setup>
+
+const name = "luis";
+</script>
+
+
 <template>
 
-  <h1>hola vue</h1>
+  <h1>hola vue {{ name.toUpperCase() }}</h1>
 </template>
+
+
+<style>
+
+h1{
+  color: red  ;
+}
+</style>
